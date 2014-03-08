ec6fe0ae240c7361008e9e1e54bf27affae6d598
Amos Stone <amos@amosarts.com>
2014-03-08 11:14:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hondo::Application.config.secret_key_base = '2d3b5c02875e592592d75efb270d6cb46c68ac2c7117bb737b94a5e6faadd3a4169e8d78ec6520cf432f5c2f4ec53a886105b32a8412341fbb6033af764876d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hondo::Application.config.session_store :cookie_store, key: '_hondo_session'
