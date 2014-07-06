241927da88e943997f41e0d1673ddbba0468b111
Amos Stone <amos@amosarts.com>
2014-07-06 15:29:10 -0700

    Partial commit, sync.

--- app/controllers/banner_controller.rb
+++ app/controllers/banner_controller.rb
@@ -3,4 +3,12 @@ class BannerController < WebsocketRails::BaseController
     # perform application setup here
     controller_store[:message_count] = 0
   end
+
+  def client_connected
+  	# send_message :welcome, {:message => 'welcome'}
+  end
+
+  def new_banner
+  	send_message :welcome, {:uid => '123'}.to_json
+  end
 end
