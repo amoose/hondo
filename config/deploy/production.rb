fa1509b704082409c1ff0882ed443a287eb69cf6
amoose <amos@amosarts.com>
2014-11-20 16:39:53 -0800

    Updates gemfiles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,13 +16,13 @@ set :rails_env, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'hondo.io', user: 'ubuntu', roles: %w{web app db}
-
+# server 'hondo.io', user: 'ubuntu', roles: %w{web app db}
+server '54.176.148.7', user: 'ubuntu', roles: %w{web app db}
 # role :resque_worker, "ec2-54-204-114-89.compute-1.amazonaws.com"
 # role :resque_scheduler, "app_domain"
 
 # set :workers, { "alerts" => 1, "queue_alerts" => 1 }
 
 set :rvm_type, :user
-set :rvm_ruby_version, '2.1.0'
+set :rvm_ruby_version, '2.1.3'
 set :rvm_user, 'ubuntu'
